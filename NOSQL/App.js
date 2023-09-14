e955d0d39644e535f2ecffcd6e7bfd3268a9c966
Mohamad Safa <mohsafa3@gmail.com>
2023-09-14 11:47:04 +0300

    node modules changes

--- NOSQL/App.js
+++ NOSQL/App.js
@@ -1,35 +1,33 @@
 //Challenge 1: Create a Basic Node.js Application
 
-const http = require('http');
+//const http = require('http');
 
-const PORT = 5000;
+//const server = http.createServer((req, res) => {
+//res.writeHead(200, { 'Content-Type': 'text/plain' });
+//res.end('Hello, Node.js!');
+//});
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, Node.js!');
-});
-
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+//server.listen(PORT, () => {
+//console.log(`Server is running on port ${PORT}`);
+//});
 
 //Challenge 2: Integrate Express.js with Node
 
 const express = require("express");
 const app = express();
+const PORT = 5000;
 
-const cors = require ("cors");
+const cors = require("cors");
 
-app.use(express.urlencoded({ extended : true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) =>{
-    console.log(`Listening at http://localhost:${port}`);
-    res.send("This is working!");
+app.get("/API", (req, res) => {
+  console.log(`Listening at http://localhost:${PORT}`);
+  res.send("This is working!");
 });
 
-app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`);
-
+app.listen(PORT, () => {
+  console.log(`Listening at http://localhost:${PORT}`);
 });
