697d1ebdd0095060bee55d0e5ed3ba7158643ec0
Mohamad Safa <mohsafa3@gmail.com>
2023-09-14 10:37:41 +0300

    Done with Challenge 2

--- NOSQL/App.js
+++ NOSQL/App.js
@@ -1,3 +1,5 @@
+//Challenge 1: Create a Basic Node.js Application
+
 const http = require('http');
 
 const PORT = 5000;
@@ -9,4 +11,25 @@ const server = http.createServer((req, res) => {
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+//Challenge 2: Integrate Express.js with Node
+
+const express = require("express");
+const app = express();
+
+const cors = require ("cors");
+
+app.use(express.urlencoded({ extended : true}));
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) =>{
+    console.log(`Listening at http://localhost:${port}`);
+    res.send("This is working!");
+});
+
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`);
+
 });
